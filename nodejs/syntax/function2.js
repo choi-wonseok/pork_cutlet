23b89363f2008e0739ef301f9278284f15bd9b99
WonseokChoi <apples226@hotmail.com>
2021-07-13 15:33:43 +0900

    Today_work

---
+++ nodejs/syntax/function2.js
@@ -0,0 +1,10 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+
+function sum(first, second){ // parameter
+  console.log('a');
+  return first+second;
+  console.log('b');
+}
+
+console.log(sum(2,4)); //각각의 입력값 argument/인자
